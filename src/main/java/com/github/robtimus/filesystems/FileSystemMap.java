250fa8dbb52de3a662cfb2f19382c67ea54daae0
Rob Spoor <rob@icemanx.nl>
2022-10-15 16:04:16 +0200

    Added FileSystemMap

---
+++ src/main/java/com/github/robtimus/filesystems/FileSystemMap.java
@@ -0,0 +1,246 @@
+/*
+ * FileSystemMap.java
+ * Copyright 2022 Rob Spoor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.robtimus.filesystems;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystemAlreadyExistsException;
+import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.spi.FileSystemProvider;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * A map for file systems that can be used by {@link FileSystemProvider} implementations.
+ * This class provides a thread-safe way to add, retrieve and remove file systems without any unnecessary locking during the actual creation of file
+ * systems, which may take a while. It does so by maintaining a lock per URI; calling {@link #get(URI)} or {@link #remove(URI)} while a file system
+ * is still being created will block until the creation is done (or has failed). However, any call with a different URI will not block.
+ *
+ * @author Rob Spoor
+ * @param <S> The type of file system to maintain.
+ * @since 2.1
+ */
+public final class FileSystemMap<S extends FileSystem> {
+
+    private final FileSystemFactory<? extends S> factory;
+
+    /*
+     * When a file system has been added, it is present in fileSystems.
+     * However, while the file system is still being created, it is present in locks instead.
+     * This allows the creation of file systems to be moved outside of global locking, and only use locking for the file system itself.
+     *
+     * The locks will be read locks, linked to a write lock that will be acquired for the duration of invocations to add. That means that once such a
+     * lock is successfully acquired, the write lock has been released and the file system will have been created.
+     *
+     * Locking strategy:
+     * - The fileSystems and locks maps are both guarded by the fileSystems map
+     * - synchronized blocks are used inside locks, but within synchronized blocks no locks are used
+     * - synchronized blocks contain only short-lived and non-blocking logic
+     */
+
+    private final Map<URI, S> fileSystems;
+    private final Map<URI, Lock> locks;
+
+    /**
+     * Creates a new {@link FileSystem} map.
+     *
+     * @param factory The factory to use to create new {@link FileSystem} instances.
+     */
+    public FileSystemMap(FileSystemFactory<? extends S> factory) {
+        this.factory = Objects.requireNonNull(factory);
+
+        fileSystems = new HashMap<>();
+        locks = new HashMap<>();
+    }
+
+    /**
+     * Adds a new file system. It is created using the factory provided in the constructor.
+     *
+     * @param uri The URI representing the file system.
+     * @param env A map of provider specific properties to configure the file system.
+     * @return The new file system.
+     * @throws NullPointerException If the given URI or map is {@code null}.
+     * @throws FileSystemAlreadyExistsException If a file system has already been added for the given URI.
+     * @throws IOException If the file system could not be created.
+     * @see FileSystemProvider#newFileSystem(URI, Map)
+     */
+    public S add(URI uri, Map<String, ?> env) throws IOException {
+        Objects.requireNonNull(uri);
+        Objects.requireNonNull(env);
+
+        Lock lock = addLock(uri);
+
+        lock.lock();
+        try {
+            S fileSystem = createFileSystem(uri, env);
+            addNewFileSystem(uri, fileSystem);
+            return fileSystem;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private Lock addLock(URI uri) {
+        synchronized (fileSystems) {
+            if (fileSystems.containsKey(uri) || locks.containsKey(uri)) {
+                throw new FileSystemAlreadyExistsException(uri.toString());
+            }
+            ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+            locks.put(uri, readWriteLock.readLock());
+            return readWriteLock.writeLock();
+        }
+    }
+
+    private S createFileSystem(URI uri, Map<String, ?> env) throws IOException {
+        try {
+            return factory.create(uri, env);
+        } catch (final Exception e) {
+            // A lock has been added as part of addLock; remove it again so add can be called again
+            removeLock(uri);
+            throw e;
+        }
+    }
+
+    @SuppressWarnings("resource")
+    private void addNewFileSystem(URI uri, S fileSystem) {
+        synchronized (fileSystems) {
+            fileSystems.put(uri, fileSystem);
+            locks.remove(uri);
+        }
+    }
+
+    private void removeLock(URI uri) {
+        synchronized (fileSystems) {
+            locks.remove(uri);
+        }
+    }
+
+    /**
+     * Returns a previously added file system.
+     *
+     * @param uri The URI representing the file system.
+     * @return The file system represented by the given URI.
+     * @throws NullPointerException If the given URI is {@code null}.
+     * @throws FileSystemNotFoundException If no file system has been added for the given URI.
+     * @see FileSystemProvider#getFileSystem(URI)
+     */
+    public S get(URI uri) {
+        Objects.requireNonNull(uri);
+
+        S fileSystem;
+        Lock lock;
+
+        synchronized (fileSystems) {
+            fileSystem = fileSystems.get(uri);
+            if (fileSystem != null) {
+                return fileSystem;
+            }
+            lock = locks.get(uri);
+            if (lock == null) {
+                throw new FileSystemNotFoundException(uri.toString());
+            }
+        }
+
+        lock.lock();
+        try {
+            // add has finished, so locks.get(uri) will be null
+            return getFileSystem(uri);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private S getFileSystem(URI uri) {
+        synchronized (fileSystems) {
+            S fileSystem = fileSystems.get(uri);
+            // Add another null check, in case remove has been called and "wins" over this call
+            if (fileSystem == null) {
+                throw new FileSystemNotFoundException(uri.toString());
+            }
+            return fileSystem;
+        }
+    }
+
+    /**
+     * Removes a previously added file system. This method should be called when a file system returned by {@link #add(URI, Map)} is closed.
+     * <p>
+     * If no file system had been added for the given URI, or if it already had been removed, no error will be thrown.
+     *
+     * @param uri The URI representing the file system.
+     * @throws NullPointerException If the given URI is {@code null}.
+     * @see FileSystem#close()
+     */
+    public void remove(URI uri) {
+        Objects.requireNonNull(uri);
+
+        Lock lock = removeFileSystemOrGetLock(uri);
+
+        if (lock != null) {
+            lock.lock();
+            try {
+                // add has finished, so locks.get(uri) will be null
+                removeFileSystem(uri);
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    @SuppressWarnings("resource")
+    private Lock removeFileSystemOrGetLock(URI uri) {
+        synchronized (fileSystems) {
+            if (fileSystems.remove(uri) != null) {
+                return null;
+            }
+            return locks.get(uri);
+        }
+    }
+
+    @SuppressWarnings("resource")
+    private void removeFileSystem(URI uri) {
+        synchronized (fileSystems) {
+            fileSystems.remove(uri);
+        }
+    }
+
+    /**
+     * A factory for file system.
+     *
+     * @author Rob Spoor
+     * @param <S> The type of file system to create.
+     */
+    public interface FileSystemFactory<S extends FileSystem> {
+
+        /**
+         * Creates a new file system.
+         *
+         * @param uri The URI representing the file system.
+         * @param env A map of provider specific properties to configure the file system.
+         * @return The created file system.
+         * @throws IOException If the file system could not be created.
+         * @see FileSystemProvider#newFileSystem(URI, Map)
+         * @see FileSystemMap#add(URI, Map)
+         */
+        S create(URI uri, Map<String, ?> env) throws IOException;
+    }
+}
---
+++ src/test/java/com/github/robtimus/filesystems/FileSystemMapTest.java
@@ -0,0 +1,333 @@
+/*
+ * FileSystemMapTest.java
+ * Copyright 2022 Rob Spoor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.robtimus.filesystems;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.net.URI;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystemAlreadyExistsException;
+import java.nio.file.FileSystemNotFoundException;
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("nls")
+class FileSystemMapTest {
+
+    private ScheduledExecutorService executor;
+
+    @BeforeEach
+    void init() {
+        executor = Executors.newScheduledThreadPool(5);
+    }
+
+    @AfterEach
+    void shutdownExecutor() {
+        executor.shutdown();
+    }
+
+    @Nested
+    @DisplayName("add")
+    class Add {
+
+        @Test
+        @DisplayName("null URI")
+        void testNullURI() {
+            @SuppressWarnings("resource")
+            FileSystem fileSystem = mock(FileSystem.class);
+            FileSystemMap<FileSystem> map = new FileSystemMap<>((uri, env) -> fileSystem);
+
+            Map<String, ?> env = Collections.emptyMap();
+
+            assertThrows(NullPointerException.class, () -> map.add(null, env));
+        }
+
+        @Test
+        @DisplayName("null env")
+        void testNullEnv() {
+            @SuppressWarnings("resource")
+            FileSystem fileSystem = mock(FileSystem.class);
+            FileSystemMap<FileSystem> map = new FileSystemMap<>((uri, env) -> fileSystem);
+
+            URI uri = URI.create("urn:test");
+
+            assertThrows(NullPointerException.class, () -> map.add(uri, null));
+        }
+
+        @Test
+        @DisplayName("file system already added")
+        void testFileSystemAlreadyAdded() {
+            @SuppressWarnings("resource")
+            FileSystem fileSystem = mock(FileSystem.class);
+            FileSystemMap<FileSystem> map = new FileSystemMap<>((uri, env) -> fileSystem);
+
+            URI uri = URI.create("urn:test");
+            Map<String, ?> env = Collections.emptyMap();
+
+            @SuppressWarnings("resource")
+            FileSystem added = assertDoesNotThrow(() -> map.add(uri, env));
+
+            assertSame(fileSystem, added);
+
+            assertThrows(FileSystemAlreadyExistsException.class, () -> map.add(uri, env));
+        }
+
+        @Test
+        @DisplayName("file system being added")
+        void testFileSystemBeingAdded() {
+            CountDownLatch createStarted = new CountDownLatch(1);
+            CountDownLatch createEnd = new CountDownLatch(1);
+
+            FileSystem fileSystem = mock(FileSystem.class);
+            FileSystemMap<FileSystem> map = new FileSystemMap<>((uri, env) -> {
+                createStarted.countDown();
+                await(createEnd);
+                return fileSystem;
+            });
+
+            URI uri = URI.create("urn:test");
+            Map<String, ?> env = Collections.emptyMap();
+
+            executor.submit(() -> map.add(uri, env));
+
+            assertTrue(assertDoesNotThrow(() -> createStarted.await(100, TimeUnit.MILLISECONDS)));
+
+            assertThrows(FileSystemAlreadyExistsException.class, () -> map.add(uri, env));
+
+            createEnd.countDown();
+        }
+
+        @Test
+        @DisplayName("factory throws exception")
+        void testFactoryThrowsException() {
+            IOException exception = new IOException();
+
+            FileSystemMap<FileSystem> map = new FileSystemMap<>((uri, env) -> {
+                throw exception;
+            });
+
+            URI uri = URI.create("urn:test");
+            Map<String, ?> env = Collections.emptyMap();
+
+            IOException thrown = assertThrows(IOException.class, () -> map.add(uri, env));
+            assertSame(exception, thrown);
+
+            // Assert that the URI is usable again
+            thrown = assertThrows(IOException.class, () -> map.add(uri, env));
+            assertSame(exception, thrown);
+        }
+    }
+
+    @Nested
+    @DisplayName("get")
+    class Get {
+
+        @Test
+        @DisplayName("null URI")
+        void testNullURI() {
+            @SuppressWarnings("resource")
+            FileSystem fileSystem = mock(FileSystem.class);
+            FileSystemMap<FileSystem> map = new FileSystemMap<>((uri, env) -> fileSystem);
+
+            assertThrows(NullPointerException.class, () -> map.get(null));
+        }
+
+        @Test
+        @DisplayName("file system not found")
+        void testFileSystemNotFound() {
+            @SuppressWarnings("resource")
+            FileSystem fileSystem = mock(FileSystem.class);
+            FileSystemMap<FileSystem> map = new FileSystemMap<>((uri, env) -> fileSystem);
+
+            URI uri = URI.create("urn:test");
+
+            assertThrows(FileSystemNotFoundException.class, () -> map.get(uri));
+        }
+
+        @Test
+        @DisplayName("file system already added")
+        void testFileSystemAlreadyAdded() {
+            @SuppressWarnings("resource")
+            FileSystem fileSystem = mock(FileSystem.class);
+            FileSystemMap<FileSystem> map = new FileSystemMap<>((uri, env) -> fileSystem);
+
+            URI uri = URI.create("urn:test");
+            Map<String, ?> env = Collections.emptyMap();
+
+            @SuppressWarnings("resource")
+            FileSystem added = assertDoesNotThrow(() -> map.add(uri, env));
+
+            assertSame(fileSystem, added);
+
+            @SuppressWarnings("resource")
+            FileSystem retrieved = map.get(uri);
+
+            assertSame(fileSystem, retrieved);
+        }
+
+        @Test
+        @DisplayName("file system being added")
+        void testFileSystemBeingAdded() {
+            CountDownLatch createEnd = new CountDownLatch(1);
+
+            FileSystem fileSystem = mock(FileSystem.class);
+            FileSystemMap<FileSystem> map = new FileSystemMap<>((uri, env) -> {
+                await(createEnd);
+                return fileSystem;
+            });
+
+            URI uri = URI.create("urn:test");
+            Map<String, ?> env = Collections.emptyMap();
+
+            executor.submit(() -> map.add(uri, env));
+            executor.schedule(createEnd::countDown, 100, TimeUnit.MILLISECONDS);
+
+            @SuppressWarnings("resource")
+            FileSystem retrieved = map.get(uri);
+
+            assertSame(fileSystem, retrieved);
+        }
+
+        @Test
+        @DisplayName("file system removed")
+        void testFileSystemRemoved() {
+            CountDownLatch createEnd = new CountDownLatch(1);
+
+            FileSystem fileSystem = mock(FileSystem.class);
+            FileSystemMap<FileSystem> map = new FileSystemMap<>((uri, env) -> {
+                await(createEnd);
+                return fileSystem;
+            });
+
+            URI uri = URI.create("urn:test");
+            Map<String, ?> env = Collections.emptyMap();
+
+            executor.submit(() -> map.add(uri, env));
+            executor.submit(() -> map.remove(uri));
+            executor.schedule(createEnd::countDown, 100, TimeUnit.MILLISECONDS);
+            // Call map.get(uri) through the executor, so the chances of race conditions with map.remove(uri) will become smaller
+            Future<FileSystem> retrieved = executor.submit(() -> map.get(uri));
+
+            ExecutionException exception = assertThrows(ExecutionException.class, retrieved::get);
+            assertInstanceOf(FileSystemNotFoundException.class, exception.getCause());
+        }
+    }
+
+    @Nested
+    @DisplayName("remove")
+    class Remove {
+
+        @Test
+        @DisplayName("null URI")
+        void testNullURI() {
+            @SuppressWarnings("resource")
+            FileSystem fileSystem = mock(FileSystem.class);
+            FileSystemMap<FileSystem> map = new FileSystemMap<>((uri, env) -> fileSystem);
+
+            assertThrows(NullPointerException.class, () -> map.remove(null));
+        }
+
+        @Test
+        @DisplayName("file system not found")
+        void testFileSystemNotFound() {
+            @SuppressWarnings("resource")
+            FileSystem fileSystem = mock(FileSystem.class);
+            FileSystemMap<FileSystem> map = new FileSystemMap<>((uri, env) -> fileSystem);
+
+            URI uri = URI.create("urn:test");
+
+            assertDoesNotThrow(() -> map.remove(uri));
+
+            assertNotAdded(map, uri);
+        }
+
+        @Test
+        @DisplayName("file system already added")
+        void testFileSystemAlreadyAdded() {
+            @SuppressWarnings("resource")
+            FileSystem fileSystem = mock(FileSystem.class);
+            FileSystemMap<FileSystem> map = new FileSystemMap<>((uri, env) -> fileSystem);
+
+            URI uri = URI.create("urn:test");
+            Map<String, ?> env = Collections.emptyMap();
+
+            @SuppressWarnings("resource")
+            FileSystem added = assertDoesNotThrow(() -> map.add(uri, env));
+
+            assertSame(fileSystem, added);
+
+            assertDoesNotThrow(() -> map.remove(uri));
+
+            assertNotAdded(map, uri);
+        }
+
+        @Test
+        @DisplayName("file system being added")
+        void testFileSystemBeingAdded() {
+            CountDownLatch createEnd = new CountDownLatch(1);
+
+            FileSystem fileSystem = mock(FileSystem.class);
+            FileSystemMap<FileSystem> map = new FileSystemMap<>((uri, env) -> {
+                await(createEnd);
+                return fileSystem;
+            });
+
+            URI uri = URI.create("urn:test");
+            Map<String, ?> env = Collections.emptyMap();
+
+            executor.submit(() -> map.add(uri, env));
+            executor.schedule(createEnd::countDown, 100, TimeUnit.MILLISECONDS);
+
+            assertDoesNotThrow(() -> map.remove(uri));
+
+            assertNotAdded(map, uri);
+        }
+
+        private void assertNotAdded(FileSystemMap<?> map, URI uri) {
+            assertThrows(FileSystemNotFoundException.class, () -> map.get(uri));
+        }
+    }
+
+    private void await(CountDownLatch latch) throws IOException {
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            InterruptedIOException exception = new InterruptedIOException();
+            exception.initCause(e);
+            throw exception;
+        }
+    }
+}
